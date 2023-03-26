2ed491d61cb228b9e98f43374c8336f7c4c984c0
palabhi017 <palabhi017@gmail.com>
2023-03-26 18:49:57 +0530

    added cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose")
 const {connection} = require("./db")
 const {productModel} = require("./Models/Productmodel")
 const {productRouter} = require("./Routes/product.route")
+const cors = require('cors');
 const app = express()
 require("dotenv").config()
-
 app.use(express.json())
+app.use(cors())
 
 app.use("/product",productRouter)
 
